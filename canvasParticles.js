261ee63d38ab9f5839f8b5cccd6ec449a0541dfa
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2022-12-10 00:35:45 +0100

    Update canvasParticles.js

--- canvasParticles.js
+++ canvasParticles.js
@@ -1,23 +1,16 @@
-// (C) 2022, Kyle Hoeckman, All rights reserved
+// Copyright (c) 2022 Kyle Hoeckman
+// https://github.com/Khoeckman/canvasParticles/blob/main/LICENSE
 
 "use strict";
 
 const Particles = function(selector, options = {}) {
   class Particles {
     constructor(selector, options = {}) {
+      var particles = this;
+
       if (document.querySelector(selector) != null) {
         this.canvas = document.querySelector(selector);
         this.ctx = this.canvas.getContext("2d");
-
-        window.onresize = () => {
-          this.resizeCanvas();
-          this.newParticles();
-        }
-
-        window.onmousemove = e => {
-          this.mouseX = e.clientX - this.rect.left + window.pageXOffset;
-          this.mouseY = e.clientY - this.rect.top + window.pageYOffset;
-        }
 
         this.options = {
           background: options.background ?? "#000",
@@ -35,6 +28,18 @@ const Particles = function(selector, options = {}) {
         this.resizeCanvas();
         this.newParticles();
         requestAnimationFrame(() => this.animation());
+
+        window.addEventListener("resize", function(e) {
+          console.log("resize", particles);
+          particles.resizeCanvas();
+          particles.newParticles();
+        });
+
+        window.addEventListener("mousemove", function(e) {
+          particles.mouseX = e.clientX - particles.canvas.offsetLeft - this.pageXOffset;
+          particles.mouseY = e.clientY - particles.canvas.offsetTop - this.pageYOffset;
+          console.log(particles.mouseY, e.clientY, particles.canvas.offsetTop, this.pageYOffset);
+        });
       }
     }
 
@@ -51,33 +56,33 @@ const Particles = function(selector, options = {}) {
       this.count = Infinity;
       this.width = this.canvas.width + this.options.connectDistance * 2;
       this.height = this.canvas.height + this.options.connectDistance * 2;
-      this.off_x = (this.canvas.width - this.width) / 2;
-      this.off_y = (this.canvas.height - this.height) / 2;
+      this.offX = (this.canvas.width - this.width) / 2;
+      this.offY = (this.canvas.height - this.height) / 2;
       this.particles = [];
 
       for (let i = 0, len = Math.floor(this.width * this.height / this.options.pixelsPerParticle); i < len; i++) {
-        let pos_x = Math.random() * this.width,
-            pos_y = Math.random() * this.height;
+        let posX = Math.random() * this.width,
+            posY = Math.random() * this.height;
 
-        this.createParticle(pos_x, pos_y);
+        this.createParticle(posX, posY);
       }
       this.len = this.particles.length;
     }
 
-    createParticle = function(pos_x, pos_y, dir, vel, size) {
-      var pos_x = pos_x - this.off_x || Math.random() * this.width,
-          pos_y = pos_y - this.off_y || Math.random() * this.height,
+    createParticle = function(posX, posY, dir, vel, size) {
+      var posX = posX - this.offX || Math.random() * this.width,
+          posY = posY - this.offY || Math.random() * this.height,
           dir = dir || Math.random() * 2 * Math.PI,
           vel = vel || .5 + Math.random() * .5,
           size = size || .5 + Math.random() ** 5 * 2;
 
       this.particles.push({
-        x: pos_x,
-        y: pos_y,
-        pos_x,
-        pos_y,
-        off_x: 0,
-        off_y: 0,
+        x: posX,
+        y: posY,
+        posX,
+        posY,
+        offX: 0,
+        offY: 0,
         dir,
         vel,
         size
@@ -90,25 +95,25 @@ const Particles = function(selector, options = {}) {
           for (let j = i + 1; j < this.len; j++) {
             let a = this.particles[i],
                 b = this.particles[j],
-                dist = Math.hypot(a.pos_x - b.pos_x, a.pos_y - b.pos_y),
-                dist_ratio = this.options.connectDistance / Math.max(dist, this.options.connectDistance / 20),
-                angle = Math.atan2(b.pos_y - a.pos_y, b.pos_x - a.pos_x);
+                dist = Math.hypot(a.posX - b.posX, a.posY - b.posY),
+                distRatio = this.options.connectDistance / Math.max(dist, this.options.connectDistance / 20),
+                angle = Math.atan2(b.posY - a.posY, b.posX - a.posX);
 
             if (dist < this.options.connectDistance / .5) {
               // apply repulsive force on all particles close together
-              a.pos_x += Math.cos(angle) * dist_ratio ** 2 * -this.options.gravity.repulsive;
-              a.pos_y += Math.sin(angle) * dist_ratio ** 2 * -this.options.gravity.repulsive;
+              a.posX += Math.cos(angle) * distRatio ** 2 * -this.options.gravity.repulsive;
+              a.posY += Math.sin(angle) * distRatio ** 2 * -this.options.gravity.repulsive;
 
-              b.pos_x += Math.cos(angle) * dist_ratio ** 2 * this.options.gravity.repulsive;
-              b.pos_y += Math.sin(angle) * dist_ratio ** 2 * this.options.gravity.repulsive;
+              b.posX += Math.cos(angle) * distRatio ** 2 * this.options.gravity.repulsive;
+              b.posY += Math.sin(angle) * distRatio ** 2 * this.options.gravity.repulsive;
 
             } else {
               // apply pulling force on all particles not close together
-              a.pos_x += Math.cos(angle) * dist_ratio ** 2 * this.options.gravity.pulling;
-              a.pos_y += Math.sin(angle) * dist_ratio ** 2 * this.options.gravity.pulling;
+              a.posX += Math.cos(angle) * distRatio ** 2 * this.options.gravity.pulling;
+              a.posY += Math.sin(angle) * distRatio ** 2 * this.options.gravity.pulling;
 
-              b.pos_x += Math.cos(angle) * dist_ratio ** 2 * -this.options.gravity.pulling;
-              b.pos_y += Math.sin(angle) * dist_ratio ** 2 * -this.options.gravity.pulling;
+              b.posX += Math.cos(angle) * distRatio ** 2 * -this.options.gravity.pulling;
+              b.posY += Math.sin(angle) * distRatio ** 2 * -this.options.gravity.pulling;
             }
           }
         }
@@ -116,27 +121,27 @@ const Particles = function(selector, options = {}) {
 
       for (let p of this.particles) {
         p.dir = (p.dir + Math.random() * .04 - .02) % (2 * Math.PI);
-        p.pos_x = (p.pos_x + Math.sin(p.dir) * p.vel % this.width + this.width) % this.width;
-        p.pos_y = (p.pos_y + Math.cos(p.dir) * p.vel % this.height + this.height) % this.height;
+        p.posX = (p.posX + Math.sin(p.dir) * p.vel % this.width + this.width) % this.width;
+        p.posY = (p.posY + Math.cos(p.dir) * p.vel % this.height + this.height) % this.height;
 
-        let off_x = p.pos_x + this.off_x - this.mouseX,
-            off_y = p.pos_y + this.off_y - this.mouseY,
-            dist_ratio = this.options.connectDistance / Math.hypot(off_x, off_y) * 2 / 3;
+        let offX = p.posX + this.offX - this.mouseX,
+            offY = p.posY + this.offY - this.mouseY,
+            distRatio = this.options.connectDistance / Math.hypot(offX, offY) * 2 / 3;
 
-        if (dist_ratio > 2 / 3) {
-          p.off_x += (dist_ratio * off_x - off_x - p.off_x) / 4;
-          p.off_y += (dist_ratio * off_y - off_y - p.off_y) / 4;
+        if (distRatio > 2 / 3) {
+          p.offX += (distRatio * offX - offX - p.offX) / 4;
+          p.offY += (distRatio * offY - offY - p.offY) / 4;
 
         } else {
-          p.off_x -= p.off_x / 4;
-          p.off_y -= p.off_y / 4;
+          p.offX -= p.offX / 4;
+          p.offY -= p.offY / 4;
         }
-        p.x = p.pos_x + p.off_x + this.off_x;
-        p.y = p.pos_y + p.off_y + this.off_y;
+        p.x = p.posX + p.offX + this.offX;
+        p.y = p.posY + p.offY + this.offY;
 
         // Make the mouse actually move the particles their position instead of just visually
-        //p.pos_x = p.x - this.off_x;
-        //p.pos_y = p.y - this.off_y;
+        //p.posX = p.x - this.offX;
+        //p.posY = p.y - this.offY;
       }
     }
 
@@ -176,7 +181,7 @@ const Particles = function(selector, options = {}) {
             }
 
             // Draws lines between the real positions of the particles
-            /*let dist = Math.hypot(a.pos_x - b.pos_x, a.pos_y - b.pos_y)
+            /*let dist = Math.hypot(a.posX - b.posX, a.posY - b.posY)
 
             if (dist < this.options.connectDistance) {
               this.ctx.strokeStyle = this.options.particleColor + (~~(Math.min(this.options.connectDistance / dist - 1, 1) * 255)).toString(16).padStart(2, 0);
@@ -192,10 +197,10 @@ const Particles = function(selector, options = {}) {
 
     isInbounds = function(p) {
       return !(
-          p.pos_x < Math.abs(this.off_x) ||
-          p.pos_x > this.width - Math.abs(this.off_x) ||
-          p.pos_y < Math.abs(this.off_y) ||
-          p.pos_y > this.height - Math.abs(this.off_y)
+          p.posX < Math.abs(this.offX) ||
+          p.posX > this.width - Math.abs(this.offX) ||
+          p.posY < Math.abs(this.offY) ||
+          p.posY > this.height - Math.abs(this.offY)
       );
     }
 
