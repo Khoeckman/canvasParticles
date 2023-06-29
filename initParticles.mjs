43e15b6a4d9dcbb22f5d530ee881955d647ab74c
Kyle Hoeckman <kyle.hoeckman@gmail.com>
2023-06-29 22:05:09 +0200

    Create initParticles.mjs

---
+++ initParticles.mjs
@@ -0,0 +1,6 @@
+import { canvasParticles } from "./canvasParticles.mjs";
+
+// Initialization
+const selector = "#canvas-particles-1"; // Query Selector for the canvas
+const options = {}; // See options
+canvasParticles(selector, options);
